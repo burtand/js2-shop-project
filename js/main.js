3f4a3bacd83264ceceaa42b892a319348f5a70c3
burtand <andrburtas@gmail.com>
2021-08-12 18:47:22 +0300

    Выполнены все задания к уроку 5
    Поле поиска ищет, отображаются результаты поиска. Для этого пришлось создать  доп массив  filtered результатов поиска, а при фетче продуктов из API массив filtered заполняется из массива продуктов.  Мне показалось это немного топорно, но ничего лучше я не придумал, подскажите это правильное решение? В случае отсутствия совпадений при поиске выдается сообщение "Такой товар не найден". 
    Корзина полноценно работает, отображается, работают методы добавления товара и удаление товара. в случае пустой корзины отображается соответствующее сообщение!
    Наконец, сообщения о пустой корзине или блоке товаров выдаются на основе компьютед свойств, проверяющих на пустые массивы результатов поиска или карзины. Это верное решение?

--- js/main.js
+++ js/main.js
@@ -4,8 +4,16 @@ const app = new Vue({
   el: '#app',
   data: {
     catalogUrl: '/catalogData.json',
+    cartUrl: '/getBasket.json',
+    removeUrl: '/deleteFromBasket.json',
+    addUrl: '/addToBasket.json',
     products: [],
-    imgCatalog: 'https://via.placeholder.com/200x150'
+    filtered: [],
+    cart: [],
+    imgCatalog: 'https://via.placeholder.com/200x150',
+    imgCart: 'https://via.placeholder.com/50x100',
+    searchLine: '',
+    isVisibleCart: false,
   },
   methods: {
     getJson(url) {
@@ -16,7 +24,59 @@ const app = new Vue({
         })
     },
     addProduct(product) {
-      console.log(product);
+      this.getJson(`${API + this.addUrl}`)
+        .then(data => {
+          if (data.result === 1) {
+            let find = this.cart.find(item => item.id_product === product.id_product);
+            if (find) {
+              find.quantity++;
+            } else {
+              let item = {
+                id_product: product.id_product,
+                price: product.price,
+                product_name: product.product_name,
+                quantity: 1
+              };
+              this.cart.push(item);
+            }
+          } else {
+            alert('Error');
+          }
+        });
+    },
+    removeProduct(product) {
+      this.getJson(`${API + this.removeUrl}`)
+        .then(data => {
+          if (data.result === 1) {
+            if (product.quantity > 1) {
+              product.quantity--
+            } else {
+              this.cart.splice(this.cart.indexOf(product), 1);
+            }
+          } else {
+            alert('Error');
+          }
+        });
+    },
+    filterGoods() {
+      const regexp = new RegExp(this.searchLine, 'i');
+      this.filtered = this.products.filter(product => regexp.test(product.product_name));
+    }
+  },
+  computed: {
+    areProductsEmpty() {
+      if (this.filtered.length === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    isCartEmpty() {
+      if (this.cart.length === 0) {
+        return true;
+      } else {
+        return false;
+      }
     }
   },
   beforeCreated() {
@@ -26,6 +86,11 @@ const app = new Vue({
     this.getJson(`${API + this.catalogUrl}`)
       .then(data => {
         this.products = data;
+        this.filtered = this.products;
+      });
+    this.getJson(`${API + this.cartUrl}`)
+      .then(data => {
+        this.cart = data.contents;
       });
   },
   beforeMount() {
