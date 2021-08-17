5e2201dab7733821afd8e04c74183586365571f0
burtand <andrburtas@gmail.com>
2021-08-17 19:44:28 +0300

    Компоненты приведены в соответствие с материалами к уроку 6

--- js/main.js
+++ js/main.js
@@ -4,16 +4,6 @@ const app = new Vue({
   el: '#app',
   data: {
     userSearch: '',
-    showCart: false,
-    catalogUrl: '/catalogData.json',
-    cartUrl: '/getBasket.json',
-    removeUrl: '/deleteFromBasket.json',
-    addUrl: '/addToBasket.json',
-    products: [],
-    cartItems: [],
-    filtered: [],
-    imgCatalog: 'https://via.placeholder.com/200x150',
-    imgCart: 'https://via.placeholder.com/50x100'
   },
   methods: {
     getJson(url) {
@@ -23,68 +13,9 @@ const app = new Vue({
           console.log(error);
         })
     },
-    addProduct(product) {
-      this.getJson(`${API + this.addUrl}`)
-        .then(data => {
-          if (data.result === 1) {
-            let find = this.cartItems.find(el => el.id_product === product.id_product);
-            if (find) {
-              find.quantity++;
-            } else {
-              let prod = Object.assign({
-                quantity: 1
-              }, product);
-              this.cartItems.push(prod)
-            }
-          } else {
-            alert('Error');
-          }
-        })
-    },
-    remove(item) {
-      this.getJson(`${API + this.removeUrl}`)
-        .then(data => {
-          if (data.result === 1) {
-            if (item.quantity > 1) {
-              item.quantity--;
-            } else {
-              this.cartItems.splice(this.cartItems.indexOf(item), 1)
-            }
-          } else {
-            alert('Error');
-          }
-        })
-    },
-    filter() {
-      let regexp = new RegExp(this.userSearch, 'i');
-      this.filtered = this.products.filter(el => regexp.test(el.product_name));
-    }
-  },
-  beforeCreated() {
-
-  },
-  created() {
-
-  },
-  beforeDestroy() {
-
-  },
-  beforeUpdate() {
-
   },
   mounted() {
-    this.getJson(`${API + this.cartUrl}`)
-      .then(data => {
-        for (let el of data.contents) {
-          this.cartItems.push(el);
-        }
-      });
-    this.getJson(`${API + this.catalogUrl}`)
-      .then(data => {
-        for (let el of data) {
-          this.products.push(el);
-          this.filtered.push(el);
-        }
-      });
+    console.log(this);
   }
 });
+
