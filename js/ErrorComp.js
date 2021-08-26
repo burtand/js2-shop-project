d73eccac240fe380bf928279dade2e0e0757711e
burtand <andrburtas@gmail.com>
2021-08-26 15:17:03 +0300

    Проект приведен в соответствие с материалами к уроку 7

--- js/ErrorComp.js
+++
@@ -1,8 +0,0 @@
-Vue.component('error', {
-  template: `
-  <div class="error" v-if="$root.errorFlag">
-    <span class="error-text">Ошибка ответа сервера!</span>
-    <button class="error-btn" @click="$root.errorFlag=!$root.errorFlag">Понятно</button>
-  </div>
-  `
-})
